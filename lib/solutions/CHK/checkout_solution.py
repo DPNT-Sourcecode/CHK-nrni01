a9b4bdba1ef7865ca5b9f08e9e765980caf6b079
sbx_user1051 <sbx_user1051@169.254.156.205>
2019-10-18 15:30:03 +0000

    Fri Oct 18 15:30:03 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,16 +1,17 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 deals_order = ['E', 'A', 'B']
-deals = {'A': {'quantity': 3, 'deal_price': 130},
-         'B': {'quantity': 2, 'deal_price': 45},
-         'E': {'quantity': 2, 'deal_price': 'B'}}
+deals = {'A': {'quantity': 3, 'deal': 130},
+         'B': {'quantity': 2, 'deal': 45},
+         'E': {'quantity': 2, 'deal': 'B'}}
 
 prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40}
 
 
 def checkout(skus):
-    # skus = skus.upper()
+
     shopping_list = {}
+
     for item in skus:
         if item not in prices:
             return -1
@@ -18,18 +19,22 @@ def checkout(skus):
             shopping_list[item] = 1
         else:
             shopping_list[item] += 1
-    total = 0
-    for item in deals_order:
-        if item in shopping_list and deals[item]['quantity']<shopping_list[item]:
-            if deals[item]
 
+    total = 0
+
+    for item in deals_order:
+        if item in shopping_list and deals[item]['quantity'] < shopping_list[item]:
+            if isinstance(deals[item]['deal'], int):
+                total += shopping_list[item]//deals[item]['quantity']*deals[item]["deal"]
+                shopping_list[item] -= shopping_list[item]//deals[item]['quantity']
+            else:
+                if deals[item]['deal'] in shopping_list:
+                    shopping_list[deals[item]['deal']] -= shopping_list[item]//deals[item]['quantity']
+                    if shopping_list[deals[item]['deal']] < 0:
+                        shopping_list[deals[item]['deal']] = 0
     for item in shopping_list:
-        # if item in deals:
-        #
-        #     total += shopping_list[item] // deals[item]['quantity'] * deals[item]['deal_price'] + shopping_list[item] % \
-        #              deals[item]['quantity'] * prices[item]
-        # else:
         total += shopping_list[item] * prices[item]
 
     return total
+
 
