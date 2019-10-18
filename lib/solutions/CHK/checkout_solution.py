c67b35d958f2f19cf27f3d043ad5695623a8dd05
sbx_user1051 <sbx_user1051@169.254.156.205>
2019-10-18 15:46:03 +0000

    Fri Oct 18 15:46:03 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,7 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 deals_order = ['E', 'A', 'B']
-deals = {'A': [{'quantity': 5, 'deal': 200}, {'quantity': 3, 'deal': 130}],
+current_deals = {'A': [{'quantity': 5, 'deal': 200}, {'quantity': 3, 'deal': 130}],
          'B': [{'quantity': 2, 'deal': 45}],
          'E': [{'quantity': 2, 'deal': 'B'}]}
 
@@ -9,8 +9,10 @@ prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40}
 
 
 def checkout(skus):
+    # a dictionary containing as key item name and as value number of items in the skus
     shopping_list = {}
 
+    # turn the skus styring into shopping_list dictionary
     for item in skus:
 
         if item not in prices:
@@ -22,28 +24,33 @@ def checkout(skus):
         else:
             shopping_list[item] += 1
 
+    # total for checkout
     total = 0
 
+    # check if the items with deals are on the shopping list
     for item in deals_order:
 
-        if item in shopping_list and deals[item]['quantity'] <= shopping_list[item]:
+        # get each deal for an item
+        for deals in current_deals[item]:
+            if item in shopping_list and deals['quantity'] <= shopping_list[item]:
 
-            if isinstance(deals[item]['deal'], int):
+                if isinstance(deals['deal'], int):
 
-                total += shopping_list[item] // deals[item]['quantity'] * deals[item]["deal"]
-                shopping_list[item] = shopping_list[item] % deals[item]['quantity']
+                    total += shopping_list[item] // deals['quantity'] * deals["deal"]
+                    shopping_list[item] = shopping_list[item] % deals['quantity']
 
-            else:
+                else:
 
-                if deals[item]['deal'] in shopping_list:
+                    if deals['deal'] in shopping_list:
 
-                    shopping_list[deals[item]['deal']] -= shopping_list[item] // deals[item]['quantity']
+                        shopping_list[deals['deal']] -= shopping_list[item] // deals['quantity']
 
-                    if shopping_list[deals[item]['deal']] < 0:
-                        shopping_list[deals[item]['deal']] = 0
+                        if shopping_list[deals['deal']] < 0:
+                            shopping_list[deals['deal']] = 0
 
     for item in shopping_list:
 
         total += shopping_list[item] * prices[item]
 
     return total
+
