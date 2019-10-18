c90838ea1e2174c7612945ff3116ce5151dac8cf
sbx_user1051 <sbx_user1051@169.254.156.205>
2019-10-18 14:48:45 +0000

    Fri Oct 18 14:48:45 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,21 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
+deals = {'A': {3: 130},
+         'B': {2: 45}}
+prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+
+
 def checkout(skus):
-    return 0
+    shopping_list = {}
+    for item in skus:
+        if item not in shopping_list:
+            shopping_list[item] = 1
+        else:
+            shopping_list[item] += 1
+    total = 0
+    for item in shopping_list:
+        if item in deals:
+            total += shopping_list[item]//
+    return total
+
 
