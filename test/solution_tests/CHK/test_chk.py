3d35ea065d97d02dd3356c174163747c9c4b7be0
sbx_user1051 <sbx_user1051@169.254.156.205>
2019-10-18 16:38:27 +0000

    Fri Oct 18 16:38:27 UTC 2019

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -32,8 +32,45 @@ class TestChk():
     def test_chk_f_deal(self):
         assert checkout_solution.checkout("FFF") == 20
 
+    def test_chk_h_deal(self):
+        assert checkout_solution.checkout("HHHHH") == 45
+
+    def test_chk_h_2nd_deal(self):
+        assert checkout_solution.checkout("HHHHHHHHHH") == 80
+
+    def test_chk_k_deal(self):
+        assert checkout_solution.checkout("KK") == 150
+
+    def test_chk_n_deal_no_m(self):
+        assert checkout_solution.checkout("NNN") == 120
+
+    def test_chk_n_deal(self):
+        assert checkout_solution.checkout("NNNMM") == 120 + 15
+
+    def test_chk_p_deal(self):
+        assert checkout_solution.checkout("PPPPP") == 200
+
+    def test_chk_Q_deal(self):
+        assert checkout_solution.checkout("QQQ") == 80
+
+    def test_chk_r_deal_no_q(self):
+        assert checkout_solution.checkout("RRR") == 150
+
+    def test_chk_r_deal(self):
+        assert checkout_solution.checkout("RRRQQ") == 150 + 30
+
+    def test_chk_u_deal(self):
+        assert checkout_solution.checkout("UUU") == 80
+
+    def test_chk_v_deal(self):
+        assert checkout_solution.checkout("VV") == 90
+
+    def test_chk_v_2nd_deal(self):
+        assert checkout_solution.checkout("VVV") == 130
+
     def test_chk(self):
         assert checkout_solution.checkout("AABCDABEEBFF") == 130 + 45 + 20 + 15 + 80 + 20
 
     def test_chk_mixed(self):
         assert checkout_solution.checkout("AABCDABABEEFFF") == 130 + 45 + 20 + 15 + 50 + 80 + 20
+
